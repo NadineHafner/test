e3088998a5d10ca9220d6709220181292b5a9cef
NadineHafner <nadine.hafner@edu.campus02.at>
2020-03-26 16:11:58 +0100

    bittte lade hoch

---
+++ HeloWorld.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class HeloWorld {
+    public static void main(String[]args){
+        Professor p= new Professor("Peter");
+        System.out.println(p.getName());
+
+
+    }
+    public static boolean tryIt(String str){
+        if(str.equals("jes")){
+            return true;
+        }
+        return false;
+    }
+    public static String myString(){
+        return "allfjieifjjoeie";
+    }
+
+}
